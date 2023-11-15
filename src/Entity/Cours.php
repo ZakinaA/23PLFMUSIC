a5747deb7317cd39f0b405c5c19180e9350a6032
HRVNoa <hervieu.noa@gmail.com>
2023-11-15 09:28:12 +0100

    creation entité Cours

---
+++ src/Entity/Cours.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CoursRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CoursRepository::class)]
+class Cours
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $libelle = null;
+
+    #[ORM\Column]
+    private ?int $ageMini = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $heureDebut = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $heureFin = null;
+
+    #[ORM\Column]
+    private ?int $nbPlaces = null;
+
+    #[ORM\Column]
+    private ?int $ageMaxi = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): static
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function getAgeMini(): ?int
+    {
+        return $this->ageMini;
+    }
+
+    public function setAgeMini(int $ageMini): static
+    {
+        $this->ageMini = $ageMini;
+
+        return $this;
+    }
+
+    public function getHeureDebut(): ?\DateTimeInterface
+    {
+        return $this->heureDebut;
+    }
+
+    public function setHeureDebut(?\DateTimeInterface $heureDebut): static
+    {
+        $this->heureDebut = $heureDebut;
+
+        return $this;
+    }
+
+    public function getHeureFin(): ?\DateTimeInterface
+    {
+        return $this->heureFin;
+    }
+
+    public function setHeureFin(?\DateTimeInterface $heureFin): static
+    {
+        $this->heureFin = $heureFin;
+
+        return $this;
+    }
+
+    public function getNbPlaces(): ?int
+    {
+        return $this->nbPlaces;
+    }
+
+    public function setNbPlaces(int $nbPlaces): static
+    {
+        $this->nbPlaces = $nbPlaces;
+
+        return $this;
+    }
+
+    public function getAgeMaxi(): ?int
+    {
+        return $this->ageMaxi;
+    }
+
+    public function setAgeMaxi(int $ageMaxi): static
+    {
+        $this->ageMaxi = $ageMaxi;
+
+        return $this;
+    }
+}
