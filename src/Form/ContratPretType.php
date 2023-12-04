eaa983180ef5e74a2a39f99c31d984379091ed53
adminsio <roussel.louane@gmail.com>
2023-12-04 17:36:18 +0100

    ajouter un Contrat de pret

---
+++ src/Form/ContratPretType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\ContratPret;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContratPretType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('dateDebut')
+            ->add('dateFin')
+            ->add('attestationAssurance')
+            ->add('etatDetailleDebut')
+            ->add('etatDetailleRetour')
+            ->add('instrument', EntityType::class, [
+                'class' => 'App\Entity\Instrument',
+                'choice_label' => function ($instrument) {
+                    return $instrument->getNumSerie();   }
+            ])
+            ->add('eleve', EntityType::class, [
+                'class' => 'App\Entity\Eleve',
+                'choice_label' => function ($eleve) {
+                    return $eleve->getNom();   }
+            ])
+            ->add('enregistrer', SubmitType::class, array('label' => 'ajout du Contrat de Pret'));
+    }
+
+
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ContratPret::class,
+        ]);
+    }
+
+}
